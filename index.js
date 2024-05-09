5fd51f5d839dec85bc4c6dfe3fb63c56b6b55beb
Ali Ashraf <aaaaaa30101975@gmail.com>
2024-05-09 17:04:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,257 @@
+// TODO Assignment-8
+  // * ===========start question: 1================
+    // var number = window.prompt("Enter a number:");
+    // console.log("The number is: " + Number(number));
+  // * ===========end question: 1================
+
+  // * ===========start question: 2================
+    // var number = window.prompt("Enter a number: ");
+    // if (number % 3 == 0 && number % 4 == 0) {
+    //     console.log(" Yes, The number is divisible by 3 and 4");
+    // }else{
+    //     console.log("No, The number is not divisible by 3 and 4");
+    // }
+  // * ===========end question: 2================
+
+  // * ===========start question: 3================
+    // var firstNumber = Number(window.prompt("Enter the first number: "));
+    // var secondNumber = Number(window.prompt("Enter the second number: "));
+    // if (firstNumber > secondNumber) {
+    //   console.log("The max number is " + firstNumber);
+    // } else if(  secondNumber > firstNumber){
+    //   console.log("The max number is " + secondNumber);
+    // }else{
+    //   console.log("The numbers are equal");
+    // }
+  // * ===========end question: 3================
+
+  // * ===========start question: 4================
+    // var number = Number(window.prompt("Enter a number: "));
+    //   if (number < 0){
+    //     console.log("The number is negative");
+    //   }else if(number > 0){
+    //     console.log("The number is positive");
+    //   }else if(number == 0){
+    //     console.log("The number is zero");
+    //   }else{
+    //     console.log("The input is not a number");
+    //   }
+  // * ===========end question: 4================
+
+  // * ===========start question: 5================
+    // var firstNumber = Number(window.prompt("Enter the first number: "));
+    // var secondNumber = Number(window.prompt("Enter the second number: "));
+    // var thirdNumber = Number(window.prompt("Enter the third number: "));
+    // if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+    //     console.log("The max number is " + firstNumber);
+    // } else if(secondNumber > firstNumber && secondNumber > thirdNumber){
+    //     console.log("The max number is " + secondNumber);
+    // }else if(thirdNumber > firstNumber && thirdNumber > secondNumber){
+    //     console.log("The max number is " + thirdNumber); 
+    // }else if(firstNumber == secondNumber == thirdNumber){
+    //     console.log("The numbers are equal");
+    // }else{
+    //     console.log("The input is not a number");
+    // }
+  // * ===========end question: 5================
+
+  // * ===========start question: 6================
+    // var Number = Number(window.prompt("Enter the  number: "));
+    // if (Number % 2 == 0){
+    //   console.log("The number is even");
+    // }else if(Number % 2 != 0){
+    //   console.log("The number is odd");
+    // }else{
+    //   console.log('input is not a number')
+    // }
+  // * ===========end question: 6================
+
+
+  // ! ========== there is no question-7  in the PDF =============
+
+
+  // * ===========start question: 8================
+    // var letter = window.prompt("Enter a letter: ");
+    // if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
+    //   console.log("The letter is a vowel");
+    // }else{
+    //   console.log("The letter is a consonant");
+    // }
+  // * ===========end question: 8================
+
+  // * ===========start question: 9================
+    // var x = window.prompt("Enter the number: "); 
+    // for (var i = 1; i <= x; i++){
+    //   console.log(i + 1);
+    // }
+  // * ===========end question: 9================
+
+  // * ===========start question: 10================
+    // var number = window.prompt('enter the number')
+    // for(var i = 1; i <= 12; i++){
+    //   console.log(number * i)
+    // }
+  // * ===========end question: 10================
+
+  // * ===========start question: 11================
+    // var number = window.prompt('enter the number')
+    // for(var i = 1; i <= number; i++){
+    //   if(i % 2 == 0){
+    //     console.log(i)
+    //   }
+    // }
+  // * ===========end question: 11================
+
+  // * ===========start question: 12================
+    // var degree_1 = Number (window.prompt('enter the degree 1'));
+    // var degree_2 = Number (window.prompt('enter the degree 2'));
+    // var degree_3 = Number (window.prompt('enter the degree 3'));
+    // var degree_4 = Number (window.prompt('enter the degree 4'));
+    // var degree_5 = Number (window.prompt('enter the degree 5'));
+    // const sum = degree_1 + degree_2 + degree_3 + degree_4 + degree_5
+    // var avg = sum / 5
+    // var percentage = (sum / 500) * 100
+    // console.log('Total marks is ' + sum);
+    // console.log('The average is ' + avg);
+    // console.log('The percentage is ' + percentage + '%');
+  // * ===========end question: 12================
+
+  // * ===========start question: 13================
+    // var number = window.prompt('enter the month number')
+    // if (number <= 12){
+    //   if(number == 4 || number == 6 || number == 9 || number == 11){
+    //     console.log("There are 30 days Days in Month")
+    //   }else if(number == 2){  
+    //     console.log("There are 28 days Days in Month")
+    //   }else{
+    //     console.log("There are 31 days 2Days in Month")
+    //   }
+    // }else{
+    //   console.log('Please enter a valid month number')
+    // }
+  // * ===========end question: 13================
+
+  // * ===========start question: 14================
+    // var degreeSub1 = Number(window.prompt('enter subject-1 degree'));
+    // var degreeSub2 = Number(window.prompt('enter subject-2 degree'));
+    // var degreeSub3 = Number(window.prompt('enter subject-3 degree'));
+    // var degreeSub4 = Number(window.prompt('enter subject-4 degree'));
+    // var degreeSub5 = Number(window.prompt('enter subject-5 degree'));
+    // var sum = degreeSub1 + degreeSub2 + degreeSub3 + degreeSub4 + degreeSub5;
+    // var percentage = (sum / 500) * 100;
+    // console.log(percentage + "%")
+    // if(percentage >= 90){
+    //   console.log("grade: A")
+    // }else if(percentage >= 80){
+    //   console.log("grade: B")
+    // }else if(percentage >= 70){
+    //   console.log("grade: C")
+    // }else if(percentage >= 60){
+    //   console.log("grade: D")
+    // }else if(percentage >= 40){
+    //   console.log("grade: E")
+    // }else if(percentage < 40){
+    //   console.log("grade: F")
+    // }
+  // * ===========end question: 14================
+
+  // * ===========start question: 15================
+  // var number = Number(window.prompt('enter month number'));
+  // switch(true){
+    //   case number == 4 || number == 6 || number == 9 || number == 11:
+    //     console.log("There are 30 days Days in Month")
+    //     break;
+    //   case number == 2:
+    //     console.log("There are 28 days Days in Month")
+    //     break;
+    //   case number == 1 || number == 3 || number == 5 || number == 7 || number == 8 || number == 10 || number == 12 :
+    //     console.log("There are 31 days Days in Month")
+    //     break;
+    //   default:
+    //     console.log('enter valid month')
+    // }
+    // * ===========end question: 15================
+    
+    // * ===========start question: 16================
+    // var char = window.prompt("enter character")
+    // switch(true){
+      //   case char == "a" || char == "e" || char == "i" || char == "o" || char == "u":
+      //     console.log("vowel");
+      //     break;
+      //   default:
+      //       console.log("consonant")
+      //     }
+      // * ===========end question: 16================
+      
+      // * ===========start question: 17================
+      // var num1 = Number(window.prompt("enter number1"));
+      // var num2 = Number(window.prompt("enter number2"));
+      // switch(true){
+        //   case num1 > num2:
+        //     console.log("number1 is bigger");
+        //     break;
+        //   case num2 > num1:
+        //     console.log("number2 is bigger");
+        //     break;
+        //   default:
+        //     console.log("both are equal")
+        // }
+        // * ===========end question: 17================
+        
+        // * ===========start question: 18================
+        // var number = Number(window.prompt('enter  number'));
+        // switch(true){
+        //   case number % 2 == 0:
+        //     console.log("number is even")
+        //     break;
+        //   case number % 2 == 1:
+        //     console.log("number is odd")
+        //     break;
+        //   default:
+        //     console.log("number is invalid")
+        // }
+        // * ===========end question: 18================
+        
+        // * ===========start question: 19================
+        // var number = Number(window.prompt('enter  number'));
+        // switch(true){
+        //   case number > 0:
+        //     console.log("number is positive")
+        //     break;
+        //   case number < 0:
+        //     console.log("number is negative")
+        //     break;
+        //   case number == 0:
+        //     console.log("number is zero")
+        //     break;
+        //   default:
+        //     console.log("number is invalid")
+        // }
+        // * ===========end question: 19================
+        
+        // * ===========start question: 20================
+      // var num1 = Number(window.prompt("enter number1"));
+      // var num2 = Number(window.prompt("enter number2"));
+      //   var operator = window.prompt("select the operator (+, -, /, *)");
+      //   var result;
+      //   switch(operator){
+      //     case "+":
+      //       result = num1 + num2
+      //       console.log("Num1 + Num2 = " + result)
+      //       break;
+      //     case "-":
+      //       result = num1 - num2
+      //       console.log("Num1 - Num2 = " + result)
+      //       break;
+      //     case "/":
+      //       result = num1 / num2
+      //       console.log("Num1 / Num2 = " + result)
+      //       break;
+      //     case "*":
+      //       result = num1 * num2
+      //       console.log("Num1 * Num2 = " + result)
+      //       break;
+      //     default:
+      //       console.log("math error")
+      //   }
+        // * ===========end question: 20================
